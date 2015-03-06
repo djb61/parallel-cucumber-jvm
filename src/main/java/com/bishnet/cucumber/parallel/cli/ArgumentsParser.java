583341634d613bec2271340b07b84e57d1cbc5aa
Dom Bishop <djb61@users.noreply.github.com>
2015-03-06 20:43:18 +0000

    Runtime configuration should be immutable so express this.

--- src/main/java/com/bishnet/cucumber/parallel/cli/ArgumentsParser.java
+++ src/main/java/com/bishnet/cucumber/parallel/cli/ArgumentsParser.java
@@ -1,7 +1,9 @@
 package com.bishnet.cucumber.parallel.cli;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.bishnet.cucumber.parallel.runtime.RuntimeConfiguration;
@@ -9,32 +11,35 @@ import com.bishnet.cucumber.parallel.runtime.RuntimeConfiguration;
 public class ArgumentsParser {
 
 	private List<String> arguments;
-	
+
 	public ArgumentsParser(List<String> arguments) {
 		this.arguments = arguments;
 	}
-	
+
 	public RuntimeConfiguration parse() {
 		List<String> cucumberArgs = new ArrayList<String>();
 		List<String> featureParseOnlyArgs = new ArrayList<String>();
 		List<String> featurePaths = new ArrayList<String>();
-		RuntimeConfiguration runtimeConfiguration = new RuntimeConfiguration();
-		runtimeConfiguration.numberOfThreads = Runtime.getRuntime().availableProcessors();
 		List<String> parseArguments = new ArrayList<String>(arguments);
+		int numberOfThreads = Runtime.getRuntime().availableProcessors();
+		boolean jsonReportRequired = false;
+		Path jsonReportPath = null;
+		boolean htmlReportRequired = false;
+		Path htmlReportPath = null;
 		while (!parseArguments.isEmpty()) {
 			String arg = parseArguments.remove(0).trim();
-			
+
 			if (arg.equals("--num-threads")) {
-				runtimeConfiguration.numberOfThreads = Integer.parseInt(parseArguments.remove(0));
+				numberOfThreads = Integer.parseInt(parseArguments.remove(0));
 			} else if (arg.equals("--plugin") || arg.equals("-p") || arg.equals("--format") || arg.equals("-f")) {
 				String pluginValue = parseArguments.remove(0);
 				String[] pluginArgsArray = pluginValue.split(":");
 				if (pluginArgsArray[0].equals("json")) {
-					runtimeConfiguration.jsonReportRequired = true;
-					runtimeConfiguration.jsonReportPath = Paths.get(pluginArgsArray[1]);
+					jsonReportRequired = true;
+					jsonReportPath = Paths.get(pluginArgsArray[1]);
 				} else if (pluginArgsArray[0].equals("html")) {
-					runtimeConfiguration.htmlReportRequired = true;
-					runtimeConfiguration.htmlReportPath = Paths.get(pluginArgsArray[1]);
+					htmlReportRequired = true;
+					htmlReportPath = Paths.get(pluginArgsArray[1]);
 				} else {
 					cucumberArgs.add(arg);
 					cucumberArgs.add(pluginValue);
@@ -56,8 +61,7 @@ public class ArgumentsParser {
 				featureParseOnlyArgs.add(parseArguments.remove(0));
 			} else if (arg.startsWith("-")) {
 				cucumberArgs.add(arg);
-			}
-			else {
+			} else {
 				featurePaths.add(arg);
 			}
 		}
@@ -65,9 +69,9 @@ public class ArgumentsParser {
 		fullFeatureParsingArguments.addAll(cucumberArgs);
 		fullFeatureParsingArguments.addAll(featureParseOnlyArgs);
 		fullFeatureParsingArguments.addAll(featurePaths);
-		runtimeConfiguration.featureParsingArguments = fullFeatureParsingArguments;
-		runtimeConfiguration.featurePaths = featurePaths;
-		runtimeConfiguration.cucumberPassthroughArguments = cucumberArgs;
+		RuntimeConfiguration runtimeConfiguration = new RuntimeConfiguration(numberOfThreads, Collections.unmodifiableList(cucumberArgs),
+				Collections.unmodifiableList(fullFeatureParsingArguments), Collections.unmodifiableList(featurePaths), htmlReportPath, htmlReportRequired,
+				jsonReportPath, jsonReportRequired);
 		return runtimeConfiguration;
 	}
 }
--- src/main/java/com/bishnet/cucumber/parallel/runtime/RuntimeConfiguration.java
+++ src/main/java/com/bishnet/cucumber/parallel/runtime/RuntimeConfiguration.java
@@ -5,13 +5,24 @@ import java.util.List;
 
 public class RuntimeConfiguration {
 
-	public int numberOfThreads;
-	public List<String> cucumberPassthroughArguments;
-	public List<String> featureParsingArguments;
-	public List<String> featurePaths;
-	public Path htmlReportPath;
-	public Path jsonReportPath;
-	public boolean htmlReportRequired;
-	public boolean jsonReportRequired;
+	public final int numberOfThreads;
+	public final List<String> cucumberPassthroughArguments;
+	public final List<String> featureParsingArguments;
+	public final List<String> featurePaths;
+	public final Path htmlReportPath;
+	public final Path jsonReportPath;
+	public final boolean htmlReportRequired;
+	public final boolean jsonReportRequired;
+	
+	public RuntimeConfiguration(int numberOfThreads, List<String> cucumberPassThroughArguments, List<String> featureParsingArguments, List<String> featurePaths, Path htmlReportPath, boolean htmlReportRequired, Path jsonReportPath, boolean jsonReportRequired) {
+		this.numberOfThreads = numberOfThreads;
+		this.cucumberPassthroughArguments = cucumberPassThroughArguments;
+		this.featureParsingArguments = featureParsingArguments;
+		this.featurePaths = featurePaths;
+		this.htmlReportPath = htmlReportPath;
+		this.htmlReportRequired = htmlReportRequired;
+		this.jsonReportPath = jsonReportPath;
+		this.jsonReportRequired = jsonReportRequired;
+	}
 
 }
